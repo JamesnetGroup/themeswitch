8cc9493e9760024413680923500acf0970760add
JAEWUNG LEE <james@jamesnet.dev>
2023-10-29 18:20:08 +0900

    Updates

--- src/ThemeSwitch/Properties/AssemblyInfo.cs
+++ src/ThemeSwitch/Properties/AssemblyInfo.cs
@@ -3,7 +3,20 @@ using System.Windows.Markup;
 
 [assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
 
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("55842684-7b4f-457c-9c70-f8fd36546764")]
 [assembly: XmlnsDefinition("https://jamesnet.dev/xaml/presentation/themeswitch", "ThemeSwitch.UI.Units")]
 [assembly: XmlnsPrefix("https://jamesnet.dev/xaml/presentation/themeswitch", "themeswitch")]
