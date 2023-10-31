1056d025e119f5ad99d8695d1e10573262f24f5d
JAEWUNG LEE <james@jamesnet.dev>
2023-10-31 17:53:26 +0900

    Updates

--- src/ThemeSwitch/UI/Units/ThemeSwitch.cs
+++ src/ThemeSwitch/UI/Units/ThemeSwitch.cs
@@ -3,6 +3,7 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 namespace ThemeSwitch.UI.Units
@@ -12,6 +13,20 @@ namespace ThemeSwitch.UI.Units
         static ThemeSwitch()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ThemeSwitch), new FrameworkPropertyMetadata(typeof(ThemeSwitch)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+
+            // Storyboard를 찾음
+            var slideOutStoryboard = this.FindResource("SlideOut") as Storyboard;
+            if (slideOutStoryboard != null)
+            {
+                // Storyboard를 사용함
+                slideOutStoryboard.Begin(this, this.Template, true);
+            }
         }
     }
 }
